3eba277257f9543b9132e0857eec77f52f0e1f88
nasir <nasirkhimani@gmail.com>
2021-08-23 15:28:00 -0400

    changed README.md to beetter outline uses.
    made search function more reliable. somtimes takes 1 min to search?

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bettermoviebox
 better movie box using sel and soap2day. 
+
+can search for tv shows but dont select them yet. Still working on it
--- search.py
+++ search.py
@@ -1,17 +1,28 @@
 from selenium import webdriver
-
+from time import sleep
+a = []
 options = webdriver.ChromeOptions()
-#options.add_argument('headless')
-
-driver = webdriver.Chrome(r'C:\Users\nasir\PycharmProjects\bettermoviebox\chromedriver.exe', options=options)
+options.add_argument('headless')
 
 
-driver.get('https://s2dfree.cc/search/keyword/breaking')
+global e
+while not a:
+    driver = webdriver.Chrome(r'C:\Users\nasir\PycharmProjects\bettermoviebox\chromedriver.exe', options=options)
+    driver.get('https://s2dfree.cc/search/keyword/breaking')
+    btn = driver.find_element_by_id('btn')
+    btn.click()
 
-btn = driver.find_element_by_id('btn')
-btn.click()
+    sleep(1)
 
-number = driver.find_elements_by_xpath('/html/body/div/div[2]/div/div[2]//*[@class = "col-lg-2 col-md-3 col-sm-4 col-xs-6 no-padding"]/div/div[2]/h5/a')
-for i in number:
-    print(i.get_property('innerHTML'))
-    
+    number = driver.find_elements_by_xpath('/html/body/div/div[2]/div/div[2]//*[@class = "col-lg-2 col-md-3 col-sm-4 col-xs-6 no-padding"]/div/div[2]/h5/a')
+    print('Loading...' % number)
+
+    for i in number:
+        a.append(i.get_property('innerHTML'))
+    e = len(number)
+
+choices = dict(zip(range(e),a))
+for i in choices.values():
+    print(i)
+choice = input('What movie do you want to watch? ')
+print(choices[int(choice)])
